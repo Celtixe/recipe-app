008f0a3f3ca258ef882bad6351f3127d380a22a9
Mikolaj Miller <mikolaj.miller@kcl.ac.uk>
2019-09-06 16:46:22 +0100

    Add enum Difficulty

--- src/main/java/com/recipe/domain/Recipe.java
+++ src/main/java/com/recipe/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,8 +26,10 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	//todo add difficulty
-	//private Difficulty difficulty;
+	
+	@Enumerated(EnumType.STRING)
+	private Difficulty difficulty;
+	
 	@Lob
 	private Byte[] image;
 	
@@ -35,6 +39,19 @@ public class Recipe {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
 	
+	
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
 	public Long getId() {
 		return id;
 	}
